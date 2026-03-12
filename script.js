ed33ceed0ac1d93e4ba884542dc5d44531d1ca09
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2026-03-12 08:23:57 +0100

    bemutatasra kesz 2.0

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// bezarja
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function (e) {
-    e.stopPropagation();
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// kipipálja
-var list = document.querySelector('#myUL');
-
-list.addEventListener('click', function (ev) {
-  var li = ev.target.closest("li");
-  if (li && !ev.target.classList.contains("close")) {
-    li.classList.toggle("checked");
-  }
-});
-
 // hozzaadas
 function newElement() {
   var li = document.createElement("li");
@@ -50,3 +29,24 @@ function newElement() {
     }
   }
 } 
+
+// bezarja
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function (e) {
+    e.stopPropagation();
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// kipipálja
+var list = document.querySelector('#myUL');
+
+list.addEventListener('click', function (ev) {
+  var li = ev.target.closest("li");
+  if (li && !ev.target.classList.contains("close")) {
+    li.classList.toggle("checked");
+  }
+});
