a0a3961ec7c4ac48edbf2601c16aae06128de3c3
Ádám <tamasi.adam.attila@diak.szbi-pg.hu>
2026-03-12 08:16:27 +0100

    bemutatásra kész

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("X");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function (e) {
-    e.stopPropagation();
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// Add a "checked" symbol when clicking on a list item
+// kipipálja
 var list = document.querySelector('#myUL');
 
 list.addEventListener('click', function (ev) {
@@ -30,7 +9,7 @@ list.addEventListener('click', function (ev) {
   }
 });
 
-// Create a new list item when clicking on the "Add" button
+// hozzaadas
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
@@ -44,6 +23,8 @@ function newElement() {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
+
+// torles X
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("X");
